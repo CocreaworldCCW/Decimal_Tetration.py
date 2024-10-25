07208363c56c45d2d61e7f852fbbf22752c5e4e8
CocreaworldCCW <andyl20110515@gmail.com>
2024-10-25 11:30:59 -0600

    Update Slog.py, more accurate

--- Slog.py
+++ Slog.py
@@ -1,18 +1,15 @@
 import math
 
-def slog(a, x, tol=1e-6, max_iter=200):
-    if x == a:
-        return 1
-    elif x == 1:
-        return 0
-    
-    guess = 1
-    for i in range(max_iter):
-        tet_guess = tetration(a, guess)
-        
-        if abs(tet_guess - x) < tol:
-            return guess
-        
-        guess -= (tet_guess - x) / (tet_guess * math.log(a) * guess)
-        
-    return None
+def super_logarithm(x, base):
+    if x < 1:
+        raise ValueError("x must be greater than or equal to 1.")
+    if base <= 1:
+        raise ValueError("base must be greater than 1.")
+    count = 0
+    while x >= base:
+        x = math.log(x, base)
+        count += 1
+    if x > 1:
+        count += math.log(x, base)
+
+    return count
